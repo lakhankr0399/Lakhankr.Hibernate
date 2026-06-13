2459b4ed2aab45f54f47f168a8968041dbd20bd1
lakhankr0399 <lakhankr031999@gmail.com>
2026-06-13 18:37:37 +0530

    update

---
+++ src/main/java/com/hbn/configuration/HibernateConfig.java
@@ -0,0 +1,41 @@
+package com.hbn.configuration;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+public class HibernateConfig {
+	
+	
+	
+	public static SessionFactory getSessionFactory()
+	{
+		
+		Properties properties = new Properties();
+		
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		
+		
+		properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/company");
+		properties.put(Environment.JAKARTA_JDBC_USER, "root");
+		properties.put(Environment.JAKARTA_JDBC_PASSWORD, "12345");
+		properties.put(Environment.HBM2DDL_AUTO, "create");
+		properties.put(Environment.SHOW_SQL, "true");
+		properties.put(Environment.FORMAT_SQL, "true");
+		
+		
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(properties).build();
+		
+		Metadata meta = new MetadataSources(ssr).addAnnotatedClass(com.hbn.entity.Employee.class).getMetadataBuilder().build();
+		
+		return meta.buildSessionFactory();
+	}
+
+}
--- src/main/java/com/hbn/main/Main.java
+++ src/main/java/com/hbn/main/Main.java
@@ -1,29 +1,30 @@
 package com.hbn.main;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.hbn.configuration.HibernateConfig;
 import com.hbn.entity.Employee;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Employee e1= new Employee(1,"vikas","male",300000,32);
-		Employee e2=new Employee(2,"Anuj","male",400000,30);
 
-		Configuration cfg=new Configuration().configure("hibernate.cfg.xml");
-		SessionFactory sf=cfg.buildSessionFactory();
-		Session session=sf.openSession();
-		Transaction tx=session.beginTransaction();
-		session.persist(e1);
-		session.persist(e2);
+		Employee e = new Employee( 0, "Vikas", "male", 330000, 40);
+
+		
+		Session session = HibernateConfig.getSessionFactory().openSession();
+
+		Transaction tx = session.beginTransaction();
+		
+		session.persist(e);
 		
 		tx.commit();
+		
+		
+		
 	}
 
 }
