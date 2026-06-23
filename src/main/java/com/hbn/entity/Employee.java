9adef2c9003e5ac63ace64f79b31a317ef3b9f89
lakhankr0399 <lakhankr031999@gmail.com>
2026-06-23 12:54:43 +0530

    update

--- src/main/java/com/hbn/entity/Employee.java
+++ src/main/java/com/hbn/entity/Employee.java
@@ -4,6 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+
+@NamedQuery(
+		name="findById",
+		query="FROM Employee E WHERE E.id> :id"
+		)
+
+@NamedQuery(
+		name="findByGender",
+		query="SELECT E FROM Employee E WHERE E.gender=:gender"
+		)
 
 @Entity
 public class Employee {
